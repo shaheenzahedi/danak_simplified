668dad8b26e4fb8088461ee5f7f49c6c98d1c29f
shaheenzahedi <shaheen.d2@gmail.com>
2023-06-07 21:22:32 +0330

    changed entry point for google jib

--- src/main/docker/jib/entrypoint.sh
+++ src/main/docker/jib/entrypoint.sh
@@ -1,7 +1,8 @@
 #!/bin/sh
 
 echo "Installing Git..."
-apk add --no-cache git
+apt-get update
+apt-get install -y git
 
 echo "The application will start in ${JHIPSTER_SLEEP}s..." && sleep ${JHIPSTER_SLEEP}
 exec java ${JAVA_OPTS} -noverify -XX:+AlwaysPreTouch -Djava.security.egd=file:/dev/./urandom -cp /app/resources/:/app/classes/:/app/libs/* "org.aydm.danak.DanakApp"  "$@"
