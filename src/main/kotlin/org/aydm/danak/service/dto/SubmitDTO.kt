116ba8cee969be32f8ded4db146e19b08f259087
shaheenzahedi <shaheen.d2@gmail.com>
2023-09-03 16:10:20 +0330

    added default constructor

--- src/main/kotlin/org/aydm/danak/service/dto/SubmitDTO.kt
+++ src/main/kotlin/org/aydm/danak/service/dto/SubmitDTO.kt
@@ -2,8 +2,8 @@ package org.web.danak.service.dto
 
 data class SubmitDTO(
     var tabletId: Long? = null,
-    val tablet: String,
-    var users: List<SubmitUserDTO>
+    val tablet: String = "",
+    var users: List<SubmitUserDTO> = emptyList()
 ) {
-    constructor(tabletId: Long?,tablet: String) : this(tabletId, tablet, emptyList())
+    constructor(tabletId: Long?, tablet: String) : this(tabletId, tablet, emptyList())
 }
