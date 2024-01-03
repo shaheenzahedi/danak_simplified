b139d44eab1d0e643c67215bdd76fa48635912c9
shaheenzahedi <shaheen.d2@gmail.com>
2024-01-03 18:40:57 +0330

    fixed problem with is empty

--- src/main/kotlin/org/aydm/danak/repository/UserActivityRepository.kt
+++ src/main/kotlin/org/aydm/danak/repository/UserActivityRepository.kt
@@ -27,12 +27,12 @@ interface UserActivityRepository : JpaRepository<UserActivity, Long>, JpaSpecifi
             "LOWER(tu.firstName) LIKE CONCAT('%', LOWER(:searchString), '%') OR " +
             "LOWER(tu.lastName) LIKE CONCAT('%', LOWER(:searchString), '%') OR " +
             "LOWER(t.name) LIKE CONCAT('%', LOWER(:searchString), '%')) " +
-            "AND (:tabletIds IS EMPTY OR t.id IN :tabletIds) " +
+            "AND (COALESCE(:tabletIds, NULL) IS NULL OR t.id IN :tabletIds) " +
             "GROUP BY tu.id"
     )
     fun getAllActivityByUserPageable(
         @Param("searchString") searchString: String?,
-        tabletIds: List<Long>,
+        @Param("tabletIds") tabletIds: List<Long>,
         pageable: Pageable?
     ): Page<Array<Any?>?>?
     fun findAllByActivityId(activityId: Long): List<UserActivity>
